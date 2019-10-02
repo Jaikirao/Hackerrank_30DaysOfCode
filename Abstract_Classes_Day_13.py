594d2e25e0b61daf988d38d248ab9f026c294219
Jaikirao <48488555+Jaikirao@users.noreply.github.com>
2019-10-03 02:35:05 +0530

    Create Abstract_Classes_Day_13.py
    hacker rank hacker earth day 13 30 day of code python abstract class

---
+++ Abstract_Classes_Day_13.py
@@ -0,0 +1,15 @@
+class MyBook:
+    def __init__(self,title , author , price):
+        self.author=author
+        self.title=title
+        self.price=price
+    def display(self):
+       print("Title:" , title)
+       print("Author:" , author)
+       print("Price:", price)
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
